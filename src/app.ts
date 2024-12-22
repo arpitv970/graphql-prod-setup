660b8e8c5625f414666d8846c2e6aeabc0b9ec4e
Arpit Verma <arpitv970@gmail.com>
2024-12-22 21:12:59 +0530

    updated backend

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,100 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import express, { Request, Response } from "express";
+import cors from "cors";
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const startServer = async () => {
+  const app = express();
+  const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
+  const server = new ApolloServer({
+    typeDefs: `
+type User {
+  id: ID!
+  name: String!
+  username: String!
+  email: String!
+  phone: String!
+  website:String!
+  todos: [Todo]
+}
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello, Express with TypeScript!" });
-});
+type Todo {
+  id: ID!
+  title: String!
+  completed: Boolean
+  user: User!
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+type Query {
+  getTodos: [Todo]
+  getAllUsers: [User]
+  getUserByID(id: ID!): User
+}
+`,
+    resolvers: {
+      User: {
+        todos: async (user) => {
+          // Fetch todos and filter by user.id
+          const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+
+          if (!res.ok) {
+            throw new Error("Failed to fetch todos");
+          }
+
+          const data = await res.json();
+
+          // Filter todos belonging to the user
+          return data.filter((todo: any) => todo.userId === user.id);
+        },
+      },
+      Query: {
+        getTodos: async () => {
+          const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+
+          if (!res.ok) {
+            throw new Error("Failed to fetch todos");
+          }
+
+          const data = await res.json();
+          return data;
+        },
+        getAllUsers: async () => {
+          const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+          if (!res.ok) {
+            throw new Error("Failed to fetch users");
+          }
+
+          const data = await res.json();
+          return data;
+        },
+        getUserByID: async (parent, { id }) => {
+          const res = await fetch(
+            `https://jsonplaceholder.typicode.com/users/${id}`,
+          );
+
+          if (!res.ok) {
+            throw new Error("Failed to fetch user");
+          }
+
+          const data = await res.json();
+          return data;
+        },
+      },
+    },
+  });
+
+  app.use(express.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(PORT, () =>
+    console.log(`GraphQL server is spinned at PORT: ${PORT}`),
+  );
+};
+
+startServer();
